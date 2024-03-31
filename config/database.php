2acd5aa94135008a448c09551e0ab4f957d83605
Sherouk Sobhy <shroukksobhy@gmail.com>
2024-03-31 19:31:27 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'tasks_management'),
             'username' => env('DB_USERNAME', 'root'),
